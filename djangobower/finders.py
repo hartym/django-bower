275fbfb7668c47cb5d12ac839797bdab23b194a9
Romain Dorgueil <romain@dorgueil.net>
2015-05-07 15:17:58 +0200

    naive implementation of a "bower" filesystem that hides everything out of "dist" when dist directory exists.

--- djangobower/finders.py
+++ djangobower/finders.py
@@ -4,6 +4,32 @@ from django.core.files.storage import FileSystemStorage
 from . import conf
 import os
 
+class BowerFileSystemStorage(FileSystemStorage):
+    def __init__(self, location=None, base_url=None, file_permissions_mode=None, directory_permissions_mode=None):
+        print location, base_url, file_permissions_mode, directory_permissions_mode
+        super(BowerFileSystemStorage, self).__init__(location, base_url, file_permissions_mode,
+                                                     directory_permissions_mode)
+
+    def listdir(self, path):
+        _path = filter(None, path.split(os.path.sep))
+        try:
+            r = super(BowerFileSystemStorage, self).listdir(path)
+            if len(_path) == 1 and 'dist' in r[0]:
+                return self.listdir(os.path.join(_path[0], 'dist'))
+        except OSError as e:
+            if len(_path) <= 1: raise
+            r = super(BowerFileSystemStorage, self).listdir(os.path.join(_path[0], 'dist', *_path[1:]))
+        return r
+
+    def path(self, name):
+        _name = name.split(os.path.sep)
+        path = None
+        if len(_name) > 1 and _name[1] != 'dist':
+            path = self.path(os.path.join(_name[0], 'dist', *_name[1:]))
+        if not path or not os.path.exists(path):
+            path = super(BowerFileSystemStorage, self).path(name)
+        return path
+
 
 class BowerFinder(FileSystemFinder):
     """Find static files installed with bower"""
@@ -18,7 +44,8 @@ class BowerFinder(FileSystemFinder):
             from ordereddict import OrderedDict
             self.storages = OrderedDict()
 
-        filesystem_storage = FileSystemStorage(location=self.locations[0][1])
+
+        filesystem_storage = BowerFileSystemStorage(location=self.locations[0][1])
         filesystem_storage.prefix = self.locations[0][0]
         self.storages[self.locations[0][1]] = filesystem_storage
 
